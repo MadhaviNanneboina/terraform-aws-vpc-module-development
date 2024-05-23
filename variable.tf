9a066b7ae323fcc7487a6ac8c07f5a5779fcba55
unknown <madhusagardasari@gmail.com>
2024-05-23 23:12:11 +0800

    changes

--- variable.tf
+++ variable.tf
@@ -78,10 +78,10 @@ variable "database_subnet_cidr_tags" {
     default = {}
 }
 
-variable "database_subnet_group_tags" {
-    type = map
-    default = {}
-}
+# variable "database_subnet_group_tags" {
+#     type = map
+#     default = {}
+# }
 
 #### Nat gateway ####
 variable "nat_gateway_tags" {
