cf881eb21a9f33594c22e5958cb819fba2a6498d
unknown <madhusagardasari@gmail.com>
2024-05-23 23:03:28 +0800

    changes

--- variable.tf
+++ variable.tf
@@ -107,18 +107,18 @@ variable "database_route_table_tags" {
     default = {}
 }
 
-#### Peering ####
-variable "is_peering_required" {
-  type = bool
-  default = false
-}
+# #### Peering ####
+# variable "is_peering_required" {
+#   type = bool
+#   default = false
+# }
 
-variable "acceptor_vpc_id" {
-  type = string
-  default = ""
-}
+# variable "acceptor_vpc_id" {
+#   type = string
+#   default = ""
+# }
 
-variable "vpc_peering_tags" {
-  type = map
-  default = {}
-}
+# variable "vpc_peering_tags" {
+#   type = map
+#   default = {}
+# }
--- vpc.tf
+++ vpc.tf
@@ -74,114 +74,114 @@ resource "aws_subnet" "database" { # first name is public[0], second name is pub
 }
 
 
-resource "aws_db_subnet_group" "default" {
-  name       = "${local.resource_name}"
-  subnet_ids = aws_subnet.database[*].id
+# resource "aws_db_subnet_group" "default" {
+#   name       = "${local.resource_name}"
+#   subnet_ids = aws_subnet.database[*].id
 
-  tags = merge(
-    var.common_tags,
-    var.database_subnet_group_tags,
-    {
-        Name = "${local.resource_name}"
-    }
-  )
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.database_subnet_group_tags,
+#     {
+#         Name = "${local.resource_name}"
+#     }
+#   )
+# }
 
-resource "aws_eip" "nat" {
-  domain   = "vpc"
-}
+# resource "aws_eip" "nat" {
+#   domain   = "vpc"
+# }
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public[0].id
 
-  tags = merge(
-    var.common_tags,
-    var.nat_gateway_tags,
-    {
-        Name = "${local.resource_name}" #expense-dev
-    }
-  )
+#   tags = merge(
+#     var.common_tags,
+#     var.nat_gateway_tags,
+#     {
+#         Name = "${local.resource_name}" #expense-dev
+#     }
+#   )
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.gw] # this is explicit dependency
-}
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.gw] # this is explicit dependency
+# }
 
-#### Public Route table ####
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+# #### Public Route table ####
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = merge(
-    var.common_tags,
-    var.public_route_table_tags,
-    {
-        Name = "${local.resource_name}-public" #expense-dev-public
-    }
-  )
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.public_route_table_tags,
+#     {
+#         Name = "${local.resource_name}-public" #expense-dev-public
+#     }
+#   )
+# }
 
-#### Private Route table ####
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+# #### Private Route table ####
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = merge(
-    var.common_tags,
-    var.private_route_table_tags,
-    {
-        Name = "${local.resource_name}-private" #expense-dev-private
-    }
-  )
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.private_route_table_tags,
+#     {
+#         Name = "${local.resource_name}-private" #expense-dev-private
+#     }
+#   )
+# }
 
-#### Database Route table ####
-resource "aws_route_table" "database" {
-  vpc_id = aws_vpc.main.id
+# #### Database Route table ####
+# resource "aws_route_table" "database" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = merge(
-    var.common_tags,
-    var.database_route_table_tags,
-    {
-        Name = "${local.resource_name}-database" #expense-dev-database
-    }
-  )
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.database_route_table_tags,
+#     {
+#         Name = "${local.resource_name}-database" #expense-dev-database
+#     }
+#   )
+# }
 
 
-#### Routes ####
-resource "aws_route" "public_route" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.gw.id
-}
+# #### Routes ####
+# resource "aws_route" "public_route" {
+#   route_table_id            = aws_route_table.public.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   gateway_id = aws_internet_gateway.gw.id
+# }
 
-resource "aws_route" "private_route_nat" {
-  route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat.id
-}
+# resource "aws_route" "private_route_nat" {
+#   route_table_id            = aws_route_table.private.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   nat_gateway_id = aws_nat_gateway.nat.id
+# }
 
-resource "aws_route" "database_route_nat" {
-  route_table_id            = aws_route_table.database.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat.id
-}
+# resource "aws_route" "database_route_nat" {
+#   route_table_id            = aws_route_table.database.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   nat_gateway_id = aws_nat_gateway.nat.id
+# }
 
-#### Route table and subnet associations ####
-resource "aws_route_table_association" "public" {
-  count = length(var.public_subnet_cidrs)
-  subnet_id      = element(aws_subnet.public[*].id, count.index)
-  route_table_id = aws_route_table.public.id
-}
+# #### Route table and subnet associations ####
+# resource "aws_route_table_association" "public" {
+#   count = length(var.public_subnet_cidrs)
+#   subnet_id      = element(aws_subnet.public[*].id, count.index)
+#   route_table_id = aws_route_table.public.id
+# }
 
-resource "aws_route_table_association" "private" {
-  count = length(var.private_subnet_cidrs)
-  subnet_id      = element(aws_subnet.private[*].id, count.index)
-  route_table_id = aws_route_table.private.id
-}
+# resource "aws_route_table_association" "private" {
+#   count = length(var.private_subnet_cidrs)
+#   subnet_id      = element(aws_subnet.private[*].id, count.index)
+#   route_table_id = aws_route_table.private.id
+# }
 
-resource "aws_route_table_association" "database" {
-  count = length(var.database_subnet_cidrs)
-  subnet_id      = element(aws_subnet.database[*].id, count.index)
-  route_table_id = aws_route_table.database.id
-}
+# resource "aws_route_table_association" "database" {
+#   count = length(var.database_subnet_cidrs)
+#   subnet_id      = element(aws_subnet.database[*].id, count.index)
+#   route_table_id = aws_route_table.database.id
+# }
